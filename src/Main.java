1c8f3b721cf6925957439f7985bc363a48873ff3
lightMari <marieswar7@gmail.com>
2026-08-25 21:08:44 +0530

    Added a boot animation

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println(Os_res.logo);
+        Os_res.bootAnimation();
         Terminal.start();
 
     }
--- src/utils/Os_res.java
+++ src/utils/Os_res.java
@@ -13,4 +13,39 @@ public class Os_res {
     public final static String  system_path = "/home/maries/Development/Java/TUX_OS/src/";
     public static String  os_path = "file_system/root/canvas/";
 
+
+
+    public static void bootAnimation() {
+            String [] animations={
+                    "Starting System.....",
+                    "Starting System check.....",
+                    "Starting Bootloader......",
+                    "Checking Hardware.....",
+                    "Checking Ram.....",
+                    "Checking Kernel.....",
+                    "Booting System process.....",
+                    "Starting Device manager......",
+                    "Starting Process Handler.....",
+                    "Starting Services......",
+                    "Booting OS......",
+                    "Checking TUI......",
+                    "Booting TUI......",
+                    "Checking Screen......",
+                    "Booting Screen......",
+                    "Booting Logo......",
+                    "Starting System......"
+            };
+
+            for(String animation:animations){
+                try {
+                    Thread.sleep(800);
+                    System.out.println(animation);
+                }
+                catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            System.out.println("-----------------------------------------");
+
+    }
 }
